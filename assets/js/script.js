b922873dff7115b03420961b305367e5d7a9aaed
unknown <rikardoan@gmail.com>
2021-10-16 22:58:22 -0400

    Weather by  day Temperature Wind Speed Humidity

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,64 +2,104 @@ var tableBody = document.getElementById('repo-table');
 var wButton = document.getElementById('fetch-button');
 var weaEl = document.getElementById('wea');
 
-
 const theKey = 'd3cead6b24ef04751594f3f9dfdaba4a';
 const theUrl = 'api.openweathermap.org/data/2.5/weather?id=';
 
-var date = moment().format("MMMM Do, YYYY, H a"); 
+var date = moment().format("MMMM Do, YYYY, H a");
+var tomorro = moment().add(1, 'days').format("MMMM Do"); 
 
+var obj = {
+
+}
 document.getElementById("Tdate").innerHTML = date;
 
 function getApi(acity) {
-  
  // const acity =  document.getElementById('inpuCity');
-
   //const theCity = acity.value.trim();
 
-  var requestUrl = 'https://api.openweathermap.org/data/2.5/weather?q='+acity+'&appid=d3cead6b24ef04751594f3f9dfdaba4a&units=imperial';
+//request city for lat and lon
 
+var requestUrlCity = 'https://api.openweathermap.org/data/2.5/forecast?q='+acity+'&appid=d3cead6b24ef04751594f3f9dfdaba4a&units=imperial';
 
+var requestUrl = 'https://api.openweathermap.org/data/2.5/weather?q='+acity+'&appid=d3cead6b24ef04751594f3f9dfdaba4a&units=imperial';
 
+ 
   fetch(requestUrl)
     .then(function (response) {
       return response.json();
+
     })
     .then(function (data) {
-      console.log(data)
-    //create elements
-        var createTableRow = document.createElement('tr');
-        var newuli = document.createElement('ul');
-      
-        var name = document.createElement('li');
-        var link = document.createElement('li');
-        var link1 = document.createElement('li');
-        var link2 = document.createElement('li');       
-    //assign data
-    
-        link1.textContent = data.main.feels_like ;
-        link1.textContent = data.main.humidity ;
-       
-//apend list  
-       /*newuli.appendChild(name);
-        newuli.appendChild(link);
-        newuli.appendChild(link1);
-        newuli.appendChild(link2);
-        */
-  //      createTableRow.appendChild(newuli);
-    //    tableBody.appendChild(createTableRow);
-      
-        var temp = data.main.temp;
-        var floorT = Math.floor(temp);
-  
-        document.getElementById("nameCity").innerHTML = "<br>" + data.name;
-    
-     // document.getElementById("dat").innerHTML = date;
-    //document.getElementById("wea").innerHTML = "<br >Temperature " + floorT;
-     //  document.getElementById("wea").innerHTML = "<br >Wind Speed " + data.wind_speed;
-        document.getElementById("hum").innerHTML =  "Humidity " + data.main.humidity ;
-        localStorage.setItem('getCity', acity);
-        localStorage.setItem('date', date);
-        localStorage.setItem('wea', date);
+   
+let latit = data.coord.lat; //take latitud from the respone
+let longi = data.coord.lon;
+console.log( latit);
+console.log(longi);
+console.log(data);
+console.log(data.name);
+let requestUrlFive = 'https://api.openweathermap.org/data/2.5/onecall?&lat='+ latit +'&lon='+ longi +'&units=imperial&exclude=alerts&appid=d3cead6b24ef04751594f3f9dfdaba4a';
+
+
+
+
+fetch(requestUrlFive)
+.then(function (response) {
+  return response.json();
+
+})
+.then(function (elwe) {
+  console.log(elwe.daily)
+  console.log(elwe.daily[1].humidity)
+
+
+ //create elements
+ var createTableRow = document.createElement('tr');
+ var newuli = document.createElement('ul');
+ var link1 = document.createElement('li');
+
+ link1.textContent = elwe.name ;
+
+
+/*newuli.appendChild(name);
+ newuli.appendChild(link);
+ newuli.appendChild(link1);
+ newuli.appendChild(link2);
+
+ */ document.getElementById("nameCity").innerHTML = data.name;
+ document.getElementById("tomo").innerHTML = tomorro;
+console.log(elwe)
+ document.getElementById("dayN").innerHTML = "Temp<br> " + Math.floor(data.main.temp)+"°";
+ document.getElementById("wea").innerHTML = "feels like <br>" + data.main.feels_like;
+ document.getElementById("hum").innerHTML += "humidity <br>" + elwe.daily[0].wind_speed;
+ document.getElementById("hum").innerHTML += "windspeed<br> " + elwe.daily[0].humidity;
+
+
+ document.getElementById("dayN2").innerHTML = "Temp <br>" +  Math.floor(elwe.daily[0].temp.day)+"°";
+ document.getElementById("wea2").innerHTML = "feels like " + elwe.daily[0].feels_like.day;
+ document.getElementById("hum2").innerHTML += "windspeed <br>" + elwe.daily[0].wind_speed;
+ document.getElementById("hum2").innerHTML += "humidity <br>" + elwe.daily[0].humidity;
+ 
+
+ 
+
+ document.getElementById("dayN3").innerHTML = "Temp " + elwe.daily[1].humidity;
+ document.getElementById("wea3").innerHTML = "feels like " + elwe.daily[1].feels_like.day;
+ document.getElementById("hum3").innerHTML += "windspeed " + elwe.daily[1].wind_speed;
+ document.getElementById("hum3").innerHTML += "humidity " + elwe.daily[1].humidity;
+ let one = `   <div class="col-sm">
+                
+ <p >DAY1</p>
+ <p >WEATHER</p>
+ <p > </p>
+</div>`
+
+ localStorage.setItem('getCity', acity);
+ localStorage.setItem('date', date);
+ localStorage.setItem('wea', date);
+
+})
+
+   
 
     });
 }
@@ -72,14 +112,10 @@ todo1.addEventListener("click", function(event, cit) {
   const theCity = acity.value.trim();
      getApi(theCity);
 
- 
-  console.log(typeof(cit)) 
+
+  
   getApi(cit);
 });
-
-
-
-
 
 
 // get tcity from the buttons
@@ -88,15 +124,13 @@ todo.addEventListener("click", function(event, cit) {
   var element = event.target;
  // alert(element+element.nodeName + todo);
   cit = element.textContent;
-  console.log(typeof(cit)) 
+
   getApi(cit);
 });
-
-
 
 // local storage
 
 var x = localStorage.getItem("getCity");
-var x = localStorage.getItem("getCity");
+
 
 console.log(x);
